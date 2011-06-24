11e79ed37fbb702f316f2664f5582dc5a961d06a
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-06-24 09:24:20 +0900

    Rails3.1: test: replace "ActionController::UrlWriter" to "Rails.application.routes.url_helpers"
    --HG--
    branch : rails3-straight-20111015-4-hg-git
    extra : transplant_source : %7BP%C7%87%F7N%91%29%2CZD%AC%C6%17%B1%A1B%A4%C8%8C

--- test/unit/lib/redmine/helpers/gantt_test.rb
+++ test/unit/lib/redmine/helpers/gantt_test.rb
@@ -22,7 +22,7 @@ class Redmine::Helpers::GanttTest < ActiveSupport::TestCase
   class GanttViewTest < ActionView::Base
     include ActionView::Helpers::UrlHelper
     include ActionView::Helpers::TextHelper
-    include ActionController::UrlWriter
+    include Rails.application.routes.url_helpers
     include ApplicationHelper
     include ProjectsHelper
     include IssuesHelper
