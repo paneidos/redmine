008c22c8159fe4a6d2cbc0518ed82de4a67e62fc
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-08-22 13:21:53 +0900

    Rails3.1: route
    Cherry-picked:
    https://github.com/finnlabs/chiliproject/commit/e9bf531facc228f5925998affc5da6c5c8f907d5
    
    --HG--
    branch : rails3-straight-20111021-1
    extra : transplant_source : %A3%21%E4%24%17ta%94%A6%EBa%B1%60C%06%BB%84f%04H

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,215 @@
 Redmine::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  root :to => 'welcome#index'
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  match '/login', :to => 'account#login', :as => :signin
+  match '/logout', :to => 'account#logout', :as => :signout
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  match '/roles/workflow/:id/:role_id/:tracker_id', :to => 'roles#workflow'
+  match '/help/:ctrl/:page', :to => 'help#index'
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  match '(projects/:project_id/(issues/:issue_id/))time_entries/report(.:format)', :to => "time_entry_reports#report", :via => [:get]
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  resources :time_entries, :to => 'timelog'
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  controller 'wikis' do
+    match 'projects/:id/wiki' => :edit, :via => [:post]
+    match 'projects/:id/wiki/destroy' => :destroy, :via => [:get, :post]
+  end
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+  controller "messages" do
+    scope 'boards/:board_id/topics', :via => [:get] do
+      match 'new' => :new
+      match ':id' => :show
+      match ':id/edit' => :edit
+    end
+    scope 'boards/:board_id/topics', :via => [:post] do
+      match 'new' => :new
+      match ':id' => :reply
+      match ':id/:action'
+    end
+  end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  controller 'boards' do
+    scope 'projects/:project_id/boards', :via => [:get] do
+      root :to => :index
+      match 'new' => :new
+      match ':id(.:format)' => :show
+      match ':id/edit' => :edit
+    end
+    post 'projects/:project_id/boards' => :new
+    post 'projects/:project_id/boards/:id/:action'
+  end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  controller 'documents' do
+    scope '(projects/:project_id/)documents', :via => [:get] do
+      root :to => :index
+      match 'new' => :new
+      match ':id' => :show
+      match ':id/edit' => :show
+    end
+    post 'projects/:project_id/documents' => :new
+    post '(projects/:project_id/)documents/:id/:action'
+  end
 
-  # See how all your routes lay out with "rake routes"
+  scope 'issues' do
+    resources :issue_moves, :only => [:new, :create], :as => 'move'
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+    # # Misc issue routes. TODO: move into resources
+    match 'auto_complete', :to => 'auto_completes#issues', :as => 'auto_complete_issues'
+    match 'preview/:id', :to => 'previews#issue', :as => 'preview_issue'
+    match 'context_menu', :to => 'context_menus#issues', :as => 'issues_context_menu'
+    controller 'journals' do
+      match 'changes' => :index, :as => 'issue_changes'
+      post ':id/quoted' => :new, :id => /\d+/, :as => 'quoted_issue'
+    end
+    controller 'issues' do
+      get 'bulk_edit' => :bulk_edit, :as => 'bulk_edit_issue'
+      post 'bulk_edit' => :bulk_update, :as => 'bulk_update_issue'
+      post ':id/destroy' => :destroy # legacy
+    end
+
+    resources :gantt, :only => [:show, :update]
+    resources :calendar, :only => [:show, :update]
+  end
+
+  scope 'projects/:project_id/issues' do
+    resources :gantt, :only => [:show, :update]
+    resources :calendar, :only => [:show, :update]
+  end
+
+  controller 'reports', :via => [:get] do
+    scope 'projects/:id/issues' do
+      match 'report' => :issue_report
+      match 'report/:detail' => :issue_report_details
+    end
+  end
+
+  controller 'issues' do
+    post 'issues' => :index
+    post 'issues/create' => :index
+  end
+
+  scope '(projects/:project_id/)' do
+    resources :issues do
+      collection do
+        post :create
+      end
+      controller :timelog do
+        resources :time_entries
+      end
+    end
+  end
+
+  controller :issue_relations, :via => [:post] do
+    scope 'issues/:issue_id/relations' do
+      match ':id' => :new
+      match ':id/destroy' => :destroy
+    end
+  end
+
+  match 'projects/:id/members/new', :to => 'members#new'
+
+  controller :users do
+    scope 'users/:id' do
+      get 'edit/:tab' => :edit, :tab => nil
+      post 'memberships' => :edit_membership
+      post 'memberships/:membership_id' => :edit_membership
+      post 'memberships/:membership_id/destroy' => :destroy_membership
+    end
+  end
+
+  resources :users, :member, :except => [:destroy] do
+    post :edit_membership
+    post :destroy_membership
+  end
+
+  # For nice "roadmap" in the url for the index action
+  match 'projects/:project_id/roadmap', :to => 'versions#index'
+
+  match 'news', :to => 'news#index', :as => 'all_news'
+  match 'news.:format', :to => 'news#index', :as => 'formatted_all_news'
+  match 'news/preview', :to => 'previews#news', :as => 'preview_news'
+  post 'news/:id/comments', :to => 'comments#create'
+  delete 'news/:id/comments/:comment_id', :to => 'comments#destry'
+
+  # map.resources :projects, :member => {
+  #   :copy => [:get, :post],
+  #   :settings => :get,
+  #   :modules => :post,
+  #   :archive => :post,
+  #   :unarchive => :post
+  # } do |project|
+  #   project.resource :project_enumerations, :as => 'enumerations', :only => [:update, :destroy]
+  #   project.resources :files, :only => [:index, :new, :create]
+  #   project.resources :versions, :collection => {:close_completed => :put}, :member => {:status_by => :post}
+  #   project.resources :news, :shallow => true
+  #   project.resources :time_entries, :controller => 'timelog', :path_prefix => 'projects/:project_id'
+
+  #   project.wiki_start_page 'wiki', :controller => 'wiki', :action => 'show', :conditions => {:method => :get}
+  #   project.wiki_index 'wiki/index', :controller => 'wiki', :action => 'index', :conditions => {:method => :get}
+  #   project.wiki_diff 'wiki/:id/diff/:version', :controller => 'wiki', :action => 'diff', :version => nil
+  #   project.wiki_diff 'wiki/:id/diff/:version/vs/:version_from', :controller => 'wiki', :action => 'diff'
+  #   project.wiki_annotate 'wiki/:id/annotate/:version', :controller => 'wiki', :action => 'annotate'
+  #   project.resources :wiki, :except => [:new, :create], :member => {
+  #     :rename => [:get, :post],
+  #     :history => :get,
+  #     :preview => :any,
+  #     :protect => :post,
+  #     :add_attachment => :post
+  #   }, :collection => {
+  #     :export => :get,
+  #     :date_index => :get
+  #   }
+
+  # end
+
+  # Destroy uses a get request to prompt the user before the actual DELETE request
+  get 'projects/:id/destroy', :to => 'projects#destroy',:as => 'project_destroy_confirm'
+  # TODO: port to be part of the resources route(s)
+  scope 'projects', :via => [:get] do
+    match ':id/settings/:tab', :to => "projects#settings"
+    match ':project_id/issues/:copy_from/copy' => "issues#new"
+  end
+
+  get '(projects/:id/)activity(.:format)', :to => "activities#index"
+
+  match 'projects/:project_id/issue_categories/new', :to => 'issue_categories#new'
+
+  controller :repositories do
+    scope 'projects/:id/repository' do
+      root :to => :show
+      get 'edit' => :edit
+      get 'statistics' => :statistics
+      get 'revisions(.:format)(/:rev)' => :revisions
+      get 'revisions/:rev/diff(.:format)' => :diff
+      get 'revisions/:rev/raw/*path' => :entry, :format => :raw, :rev => /[a-z0-9\.\-_]+/
+      get 'revisions/:rev/:action/*path' => :entry, :format => :raw, :rev => /[a-z0-9\.\-_]+/
+      get 'raw/*path' => :entry, :format => :raw
+      # TODO: why the following route is required?
+      get 'entry/*path' => :entry
+      get ':action/*path'
+
+      post ':action'
+    end
+  end
+
+  scope 'attachments/:id', :id => /\d+/ do
+    controller :attachments do
+      match '(:filename)' => :show
+    end
+  end
+  match 'attachments/download/:id/:filename', :to => 'attachments#download', :id => /\d+/
+
+  resources :groups
+
+  controller :sys do
+    get 'sys/projects.:format' => :projects
+    post 'sys/projects/:id/repository.:format' => :create_project_repository
+  end
+
+  # Install the default route as the lowest priority.
+  match '/:controller(/:action(/:id))'
+  match 'robots.txt', :to => 'welcome#robots'
+  # Used for OpenID
+  root :to  => 'account#login'
 end
