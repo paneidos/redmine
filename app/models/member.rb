770fc96caaa14187cf23fe182befc6995d966d51
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-08-31 15:34:29 +0900

    Rails3: model: pending: member.rb "validate"
    There is no test.
    
    --HG--
    branch : rails3-straight-20111015-4-hg-git
    extra : transplant_source : qs%2BS%CE%FB%E5%F7%160%89eh%B6%E7_%CDz%80%2A

--- app/models/member.rb
+++ app/models/member.rb
@@ -24,6 +24,7 @@ class Member < ActiveRecord::Base
 
   validates_presence_of :principal, :project
   validates_uniqueness_of :user_id, :scope => :project_id
+  validate :validate_role
 
   before_destroy :remove_category
   after_destroy :unwatch_from_permission_change
@@ -82,7 +83,7 @@ class Member < ActiveRecord::Base
 
   protected
 
-  def validate
+  def validate_role
     errors.add_on_empty :role if member_roles.empty? && roles.empty?
   end
 
